d3ea70f2e7c560f2b371d4d400af057c87cac04f
Philip Bauer <bauer@starzel.de>
2014-12-07 15:39:46 +0100

    add egg

---
+++ src/plone.dojo/setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""Installer for the plone.dojo package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+
+setup(
+    name='plone.dojo',
+    version='0.1',
+    description="A demo-addon for a plone-dojo",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3.4",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='Plone Python',
+    author='Philip Bauer',
+    author_email='bauer@starzel.de',
+    url='http://pypi.python.org/pypi/plone.dojo',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'setuptools',
+        'z3c.jbot',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.robotframework',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
